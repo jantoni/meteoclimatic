05f6c81e2bd447d0983d1e1ab4f4ea9c7e00c45c
jantoni <ea4dqx@gmail.com>
2026-04-26 10:40:00 +0200

    Update api-meteoclimatic.py

--- api-meteoclimatic.py
+++ api-meteoclimatic.py
@@ -5,24 +5,33 @@
 
 # Este Script envía los datos de tu estación a Meteoclimatic utilizando la versión 3 de su API.
 # Para ello necesitas definir 2 variables
-# api_key define el ID API (o API Key) del usuario, no de la estación. Lo puedes encontrar en la sección "Perfil" de Meteoclimatic una vez identificado
-# datafile define el fichero donde este script debe encontrar los datos a enviar a Meteoclimatic. Debes indicar la ruta y el nombre del fichero
+# - api_key define el ID API (o API Key) del usuario, no de la estación. Lo puedes encontrar en la sección "Perfil" de Meteoclimatic una vez identificado
+# - datafile define el fichero donde este script debe encontrar los datos a enviar a Meteoclimatic. Debes indicar la ruta y el nombre del fichero
+#
+# Para que funcione este script necesitas darle permisos de ejecución con chmod o bien invocarlo con el comando python3
+# sudo python3 /etc/weewx/api-meteoclimatic.py
+# 
+# Para que envíe datos regularmente deberás añadir una línea al fichero /etc/crontab (o al fichero de cron que elijas)
+# Recomendamos enviar datos cada 5 minutos, por lo que la línea de /etc/crontab quedaría así:
+# */5 * * * * root sleep 50 && bash /etc/weewx/api-meteoclimatic.sh
+# Explicación: Ejecuta cada 5 minutos el comando sleep 50, lo que provoca un retraso de 50 segundos para que la generación del fichero
+# con los datos sea finalizado por tu software. A continuación de los 50 segundos ejecuta el script.
 
 
-
-
+###########################
 ### VARIABLES A DEFINIR ###
-###########################################################################################
+###########################
+
 # Tu apikey Meteoclimatic. Podrás verlo en tu perfil, con el nombre "Identificador de API"
 api_key = "TU_API_KEY"
 
 # La ruta y el nombre del fichero que contiene los datos
 datafile = "PATH_Y_NOMBRE_FICHERO_DATOS"
-###########################################################################################
 
 
-
+############################################
 ### A PARTIR DE AQUÍ NO DEBES TOCAR NADA ###
+############################################
 
 url = "https://api.m11c.net/v3/station/wxupdate"
 
